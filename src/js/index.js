e4881e80dd7a88258c5f85b4b2a868a8dfc16943
Catherine Hansen <cat.hansen.w@gmail.com>
2021-10-27 14:16:00 -0600

    another test

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,9 @@ import { createStore } from "./redux/store";
 import { dataFetcher } from "./utils/dataFetcher";
 import { Router } from "./router/router";
 
+const app = document.querySelector("#app")
+app.innerHTML = "<h1>Is this working?</h1>"
+
 
 const storeInit = async function (e) {
     let toDoList = await dataFetcher('toDo.json')
